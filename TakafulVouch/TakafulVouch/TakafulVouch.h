820c434b32c72caa7c5f992606a3ae4a0c6d091b
sharjeelahmed <sharjeelahmed92@hotmail.com>
2020-05-28 15:24:19 +0500

    initial initialization

---
+++ TakafulVouch/TakafulVouch/TakafulVouch.h
@@ -0,0 +1,19 @@
+//
+//  TakafulVouch.h
+//  TakafulVouch
+//
+//  Created by Shairjeel Ahmed on 28/05/2020.
+//  Copyright © 2020 Shairjeel Ahmed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TakafulVouch.
+FOUNDATION_EXPORT double TakafulVouchVersionNumber;
+
+//! Project version string for TakafulVouch.
+FOUNDATION_EXPORT const unsigned char TakafulVouchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TakafulVouch/PublicHeader.h>
+
+
